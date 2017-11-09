5577ff7bf3c98aa10a27123cddd79be0a21b80d0
NamHwaSu <namhs93@ajou.ac.kr>
2017-11-09 15:33:02 +0900

    test123bb

--- AdminActivity.java
+++ AdminActivity.java
@@ -22,7 +22,7 @@ import java.util.ArrayList;
 
 public class AdminActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
 
-    //숮ㅇ
+    //Test123213
     private ListView mListView;
     //데이터 리스트
     private ArrayList<String> mArrayList;
